b49bfd4996695e20bdab10f999f8dafcbbc66bda
Jonathan Kim <vizard424@yahoo.com>
2022-08-24 16:26:20 -0600

    updated header with brand statement, name; modified formatting

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import './header.css';
-import people from '../../assets/people.png';
 import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div className='gpt3__header section__padding' id='home'>
-      <div className='gpt3__header-content'>
-        <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, enim, eius consectetur quidem ea harum ducimus doloribus suscipit dolor, nihil tenetur numquam! Est eveniet repellat ad cum voluptate veritatis voluptas!</p>
-        <div className='gpt3__header-content__input'>
-          <input type='email' placeholder='Your email address' />
-          <button type='button'>Get Started</button>
+    <div className='portfolio__header section__padding' id='home'>
+      <div className='portfolio__header-content'>
+        <div className='portfolio__header-content__title'>
+          <h1 className='gradient__text'>Jonathan Kim</h1>
+          <h3 className='gradient__text'>Full Stack Developer</h3>
         </div>
-        <div className='gpt3__header-content__people'>
-          <img src={people} alt='people' />
-          <p>1,600 people requested access to vist in last 24 hours</p>
+        <h2>Simplicity is the ultimate sophistication</h2>
+        <h4>-Leonardo da Vinci</h4>
+        <p>Full stack web developer coming from a mechanical engineering background focused on the automotive industry. Passionate about programming innovative solutions and developing unique apps that progress new technologies in a continually evolving industry. Excited to use my skills and leverage my background with an agile team to build impactful websites and apps.</p>
+        <div className='portfolio__header-content__input'>
+          <a href='/upload/jonathan_kim_fullStackDev_resume.pdf' target='_blank' rel='noopener noreferrer'>Resume</a>
         </div>
       </div>
-      <div className='gpt3__header-image'>
+      <div className='portfolio__header-image'>
         <img src={ai} alt='ai' />
       </div>
     </div>
