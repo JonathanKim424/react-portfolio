49274af94f9c8b64283803c50cd8a205aca4f840
JonathanKim424 <vizard424@yahoo.com>
2022-08-26 09:42:51 -0600

    updated navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,47 +1,38 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/jk-logo_white1.png';
 
 const Menu = () => (
   <>
-  <p><a href='#home'>Home</a></p>
-  <p><a href='#wgpt3'>What is GPT3</a></p>
-  <p><a href='#possibility'>Open AI</a></p>
-  <p><a href='#features'>Case Studies</a></p>
-  <p><a href='#blog'>Library</a></p>
+  <p><a href='#home'>About Me</a></p>
+  <p><a href='#home'>Skills</a></p>
+  <p><a href='#home'>Projects</a></p>
+  <p><a href='#home'>Contact Me</a></p>
   </>
 )
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div className='gpt3__navbar'>
-      <div className='gpt3__navbar-links'>
-        <div className='gpt3__navbar-links_logo'>
+    <div className='portfolio__navbar'>
+      <div className='portfolio__navbar-links'>
+        <div className='portfolio__navbar-links_logo'>
           <img src={logo} alt='logo' />
         </div>
-        <div className='gpt3__navbar-links_container'>
+        <div className='portfolio__navbar-links_container'>
           <Menu />
         </div>
       </div>
-      <div className='gpt3__navbar-sign'>
-        <p>Sign In</p>
-        <button type='button'>Sign Up</button>
-      </div>
-      <div className='gpt3__navbar-menu'>
+      <div className='portfolio__navbar-menu'>
         {toggleMenu
           ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
         }
         {toggleMenu && (
-          <div className='gpt3__navbar-menu_container scale-up-center'>
-            <div className='gpt3__navbar-menu_container-links'>
+          <div className='portfolio__navbar-menu_container scale-up-center'>
+            <div className='portfolio__navbar-menu_container-links'>
               <Menu />
-              <div className='gpt3__navbar-menu_container-links-sign'>
-                <p>Sign In</p>
-                <button type='button'>Sign Up</button>
-              </div>
             </div>
           </div>
         )}
