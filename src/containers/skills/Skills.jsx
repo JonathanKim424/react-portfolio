24107b4fd8faed2c1d113a93ed80ee9513b87a48
JonathanKim424 <vizard424@yahoo.com>
2022-08-26 22:37:28 -0600

    modified skills section

--- src/containers/skills/Skills.jsx
+++ src/containers/skills/Skills.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import './skills.css';
+import frontEndLogo from '../../assets/front_end-logos_transparent1.png';
 
 const Skills = () => {
     return (
         <div className='portfolio__skills section__padding' id='skills'>
             <div className='portfolio__skills-heading'>
-                <h1 className='gradient__text'>Full Stack Developer with a fullstack of skills</h1>
+                <h1 className='gradient__text'>Full Stack Developer with a full stack of skills</h1>
             </div>
             <div className='portfolio__skills-content'>
                 <div className='portfolio__skills-content_div'>
-                    <h4>Front End</h4>
-                    <p>HTML5</p>
-                    <p>CSS</p>
-                    <p>JavaScript</p>
-                    <p>React</p>
-                    <p>Bootstrap</p>
-                    <p>Tailwind</p>
+                    <div className='portfolio__skills-content_div-logo'>
+                        <img src={frontEndLogo} alt='front end logo' />
+                    </div>
+                    <div className='portfolio__skills-content_div-list'>
+                        <p>HTML5</p>
+                        <p>CSS</p>
+                        <p>JavaScript</p>
+                    </div>
+                    <div className='portfolio__skills-content_div-list'>
+                        <p>React</p>
+                        <p>Bootstrap</p>
+                        <p>Tailwind</p>
+                    </div>
                 </div>
                 <div className='portfolio__skills-content_div'>
                     <h4>Back End</h4>
@@ -25,6 +32,8 @@ const Skills = () => {
                     <p>REST API</p>
                     <p>GraphQL</p>
                 </div>
+            </div>
+            <div className='portfolio__skills-content'>
                 <div className='portfolio__skills-content_div'>
                     <h4>Databases</h4>
                     <p>MySQL</p>
