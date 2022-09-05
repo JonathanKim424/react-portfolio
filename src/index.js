85cb544d655092812c19cb2d6acfa03cee69862d
JonathanKim424 <vizard424@yahoo.com>
2022-09-04 23:20:26 -0600

    added default service worker files

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
